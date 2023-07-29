0f38930a691055cdcb7cd4b6e85f32c3e8f5f3aa
sanghmitra2023 <128592793+sanghmitra2023@users.noreply.github.com>
2023-07-29 11:19:23 +0000

    finish back and front integration

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,17 +1,24 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Context } from "../store/appContext";
 
 export const Navbar = () => {
+
+	const { actions } = useContext(Context)
+	const location = useLocation();
+	const navigate = useNavigate()
+
+	const logoutCondition = location.pathname !== "/login" && location.pathname !== "/" && location.pathname !== "/signup"
+
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<span className="navbar-brand mb-0 h1">Welcome!</span>
 				</Link>
+				
 				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+					{logoutCondition && <button onClick={() => actions.logOut(navigate)} className="btn btn-primary">Log Out</button>}
 				</div>
 			</div>
 		</nav>
